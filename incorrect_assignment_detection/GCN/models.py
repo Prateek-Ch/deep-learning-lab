595da1c70fbb4f0a89f0127f9ac97933c4058635
PrateekCh <prateekchaturvedi216@gmail.com>
2024-06-27 16:19:10 +0200

    batch norms - 58 auc

--- incorrect_assignment_detection/GCN/models.py
+++ incorrect_assignment_detection/GCN/models.py
@@ -1,22 +1,32 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, BatchNorm
 
 class GCNModel(nn.Module):
-    def __init__(self, num_features, hidden_channels):
+    def __init__(self, num_features, hidden_channels, num_layers, dropout):
         super(GCNModel, self).__init__()
-        self.conv1 = GCNConv(num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.num_layers = num_layers
+        self.convs = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        self.convs.append(GCNConv(num_features, hidden_channels))
+        self.batch_norms.append(BatchNorm(hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels))
+            self.batch_norms.append(BatchNorm(hidden_channels))
+        self.convs.append(GCNConv(hidden_channels, hidden_channels))
+        self.batch_norms.append(BatchNorm(hidden_channels))
         self.fc = nn.Linear(hidden_channels, 1)
+        self.dropout = dropout
 
     def forward(self, x, edge_index):
-
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
+        for i, (conv, batch_norm) in enumerate(zip(self.convs, self.batch_norms)):
+            if i > 0:
+                x = x + conv(x, edge_index)  # Residual connection
+            else:
+                x = conv(x, edge_index)
+            x = batch_norm(x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.fc(x)
-
+        
         return F.sigmoid(x)
